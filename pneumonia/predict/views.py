92a8d58487b99edde0c51f85a76d47e580901424
Ke Thien <kethien.ng@gmail.com>
2021-08-26 16:53:09 +0200

    ajax fetch on submit

--- pneumonia/predict/views.py
+++ pneumonia/predict/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def predict(request):
     return render(request, 'predict.html')
+
+
+def compute(request):
+    return JsonResponse({'id': '1', 'image_url': 'something', 'timestamp': '12/01/2021 10:00', 'result': '0.5', })
